d61aeff7d47b0cdaf810b105c8b94ccb033903b4
markojokic27 <jokicmarko98@gmail.com>
2023-06-04 23:42:23 +0200

    dodani upiti

--- NBA.sql
+++ NBA.sql
@@ -608,8 +608,525 @@ VALUES
   (449, 'Anthony Gill', 30),
   (450, 'Jordan Bell', 30);
 
-SELECT * FROM Igrac
-	WHERe IdTim = 1
+INSERT INTO Trener (id, ime, IdTim)
+VALUES
+(1, 'Nate McMillan', 1), 
+(2, 'Ime Udoka', 2),
+(3, 'Steve Nash', 3), 
+(4, 'James Borrego', 4), 
+(5, 'Billy Donovan', 5), 
+(6, 'J.B. Bickerstaff', 6), 
+(7, 'Jason Kidd', 7), 
+(8, 'Michael Malone', 8), 
+(9, 'Dwane Casey', 9), 
+(10, 'Steve Kerr', 10), 
+(11, 'Stephen Silas', 11), 
+(12, 'Rick Carlisle', 12), 
+(13, 'Tyronn Lue', 13), 
+(14, 'Frank Vogel', 14), 
+(15, 'Taylor Jenkins', 15), 
+(16, 'Erik Spoelstra', 16), 
+(17, 'Mike Budenholzer', 17), 
+(18, 'Chris Finch', 18), 
+(19, 'Willie Green', 19), 
+(20, 'Tom Thibodeau', 20), 
+(21, 'Mark Daigneault', 21), 
+(22, 'Jamahl Mosley', 22), 
+(23, 'Doc Rivers', 23), 
+(24, 'Monty Williams', 24), 
+(25, 'Chauncey Billups', 25), 
+(26, 'Luke Walton', 26), 
+(27, 'Gregg Popovich', 27), 
+(28, 'Nick Nurse', 28), 
+(29, 'Quin Snyder', 29), 
+(30, 'Scott Brooks', 30); 
+
+INSERT INTO Sudac (id, ime)
+VALUES
+(1, 'John Goble'),
+(2, 'Marc Davis'),
+(3, 'Tony Brothers'),
+(4, 'Kane Fitzgerald'),
+(5, 'Scott Foster'),
+(6, 'Tom Washington'),
+(7, 'Courtney Kirkland'),
+(8, 'Ken Mauer'),
+(9, 'Sean Wright'),
+(10, 'James Capers');
+
+INSERT INTO Utakmica (Id, IdDomacin, IdGost, IdSudac, Datum, PoeniDomacina, PoeniGosta, FazaNatjecanja)
+VALUES
+(1, 1, 2, 1, '2023-10-15 19:00:00', 105, 98, 'Regularna sezona'),
+(2, 3, 4, 2, '2023-10-16 20:30:00', 110, 112, 'Regularna sezona'),
+(3, 5, 6, 3, '2023-10-17 18:00:00', 92, 89, 'Regularna sezona'),
+(4, 7, 8, 4, '2023-10-18 19:15:00', 115, 110, 'Regularna sezona'),
+(5, 9, 10, 5, '2023-10-19 20:00:00', 100, 105, 'Regularna sezona'),
+(6, 11, 12, 6, '2023-10-20 18:30:00', 108, 99, 'Regularna sezona'),
+(7, 13, 14, 7, '2023-10-21 19:45:00', 120, 112, 'Regularna sezona'),
+(8, 15, 16, 8, '2023-10-22 20:15:00', 105, 98, 'Regularna sezona'),
+(9, 17, 18, 9, '2023-10-23 18:00:00', 92, 96, 'Regularna sezona'),
+(10, 19, 20, 10, '2023-10-24 19:30:00', 110, 108, 'Regularna sezona'),
+(11, 21, 22, 1, '2023-10-25 20:00:00', 105, 100, 'Regularna sezona'),
+(12, 23, 24, 2, '2023-10-26 19:30:00', 112, 108, 'Regularna sezona'),
+(13, 25, 26, 3, '2023-10-27 18:45:00', 98, 95, 'Regularna sezona'),
+(14, 27, 28, 4, '2023-10-28 20:15:00', 115, 110, 'Regularna sezona'),
+(15, 29, 30, 5, '2023-10-29 19:00:00', 105, 102, 'Regularna sezona'),
+(16, 1, 3, 6, '2023-10-30 18:30:00', 108, 112, 'Regularna sezona'),
+(17, 5, 7, 7, '2023-10-31 19:45:00', 120, 116, 'Regularna sezona'),
+(18, 9, 11, 8, '2023-11-01 20:30:00', 105, 98, 'Regularna sezona'),
+(19, 13, 15, 9, '2023-11-02 18:15:00', 92, 95, 'Regularna sezona'),
+(20, 17, 19, 10, '2023-11-03 19:00:00', 110, 108, 'Regularna sezona'),
+(21, 8, 9, 1, '2023-04-15 19:00:00', 112, 108, 'Playin'),
+(22, 10, 11, 2, '2023-04-15 20:30:00', 98, 102, 'Playin'),
+(23, 1, 8, 3, '2023-04-17 19:00:00', 105, 98, 'Prva runda'),
+(24, 2, 7, 4, '2023-04-18 20:30:00', 110, 112, 'Prva runda'),
+(25, 3, 6, 5, '2023-04-19 18:00:00', 92, 89, 'Prva runda'),
+(26, 4, 5, 6, '2023-04-20 19:15:00', 115, 110, 'Prva runda'),
+(27, 1, 4, 7, '2023-04-22 20:00:00', 100, 105, 'Polufinale konferencije'),
+(28, 3, 7, 8, '2023-04-24 18:30:00', 108, 99, 'Polufinale konferencije'),
+(29, 1, 3, 9, '2023-04-26 19:45:00', 120, 112, 'Finale konferencije'),
+(30, 7, 3, 10, '2023-04-28 20:15:00', 105, 98, 'Finale konferencije'),
+(31, 1, 7, 1, '2023-05-01 18:00:00', 92, 96, 'Finale');
+
+INSERT INTO Zamjena(Id,IdUtakmica,IdIgracUlazi,IdIgracIzlazi,Minuta)
+VALUES
+(1,1,1,NULL,0),
+(2,1,2,NULL,0),
+(3,1,3,NULL,0),
+(4,1,4,NULL,0),
+(5,1,5,NULL,0),
+(6,1,16,NULL,0),
+(7,1,17,NULL,0),
+(8,1,18,NULL,0),
+(9,1,19,NULL,0),
+(10,1,20,NULL,0),
+(11,1,8,1,30),
+(12,1,9,2,30),
+(13,1,29,19,20),
+(14,1,25,18,40),
+(15,1,1,8,45),
+(16,1,2,9,45),
+(17,1,19,29,45),
+(18,1,18,25,45),
+(19,1,NULL,1,48),
+(20,1,NULL,2,48),
+(21,1,NULL,3,48),
+(22,1,NULL,4,48),
+(23,1,NULL,5,48),
+(24,1,NULL,16,48),
+(25,1,NULL,17,48),
+(26,1,NULL,18,48),
+(27,1,NULL,19,48),
+(28,1,NULL,20,48);
+INSERT INTO Zamjena(Id,IdUtakmica,IdIgracUlazi,IdIgracIzlazi,Minuta)
+VALUES
+(29,2,31,NULL,0),
+(30,2,32,NULL,0),
+(31,2,33,NULL,0),
+(32,2,34,NULL,0),
+(33,2,35,NULL,0),
+(34,2,46,NULL,0),
+(35,2,47,NULL,0),
+(36,2,48,NULL,0),
+(37,2,49,NULL,0),
+(38,2,50,NULL,0),
+(39,2,38,31,30),
+(40,2,39,32,30),
+(41,2,59,49,20),
+(42,2,55,48,40),
+(43,2,31,38,45),
+(44,2,32,39,45),
+(45,2,49,59,45),
+(46,2,48,58,45),
+(47,2,NULL,31,48),
+(48,2,NULL,32,48),
+(49,2,NULL,33,48),
+(50,2,NULL,34,48),
+(51,2,NULL,35,48),
+(52,2,NULL,46,48),
+(53,2,NULL,47,48),
+(54,2,NULL,48,48),
+(55,2,NULL,49,48),
+(56,2,NULL,50,48);
+INSERT INTO Zamjena(Id,IdUtakmica,IdIgracUlazi,IdIgracIzlazi,Minuta)
+VALUES
+(57,31,1,NULL,0),
+(58,31,3,NULL,0),
+(59,31,5,NULL,0),
+(60,31,7,NULL,0),
+(61,31,9,NULL,0),
+(63,31,91,NULL,0),
+(64,31,92,NULL,0),
+(65,31,93,NULL,0),
+(66,31,94,NULL,0),
+(67,31,95,NULL,0),
+(68,31,2,1,30),
+(69,31,4,3,30),
+(70,31,100,91,20),
+(71,31,101,92,40),
+(72,31,1,2,45),
+(73,31,3,4,45),
+(74,31,91,100,45),
+(75,31,92,101,45),
+(76,31,NULL,1,48),
+(77,31,NULL,3,48),
+(78,31,NULL,5,48),
+(79,31,NULL,7,48),
+(80,31,NULL,9,48),
+(81,31,NULL,91,48),
+(82,31,NULL,92,48),
+(83,31,NULL,93,48),
+(84,31,NULL,94,48),
+(85,31,NULL,95,48);
+
+INSERT INTO Sut(Id,IdUtakmica,IdIgrac,Vrsta,Kos)
+VALUES
+(1,1,1,1,TRUE),
+(2,1,1,1,TRUE),
+(3,1,1,2,FALSE),
+(4,1,1,3,TRUE),
+(5,1,1,3,FALSE),
+(6,1,3,1,FALSE),
+(7,1,3,1,TRUE),
+(8,1,3,2,TRUE),
+(9,1,3,2,TRUE),
+(10,1,3,3,TRUE),
+(11,1,16,1,TRUE),
+(12,1,16,1,TRUE),
+(13,1,16,2,FALSE),
+(14,1,16,2,FALSE),
+(15,1,16,3,TRUE),
+(16,1,17,2,TRUE),
+(17,1,17,2,TRUE),
+(18,1,17,3,TRUE),
+(19,1,17,3,FALSE),
+(20,1,17,3,TRUE);
+INSERT INTO Sut(Id,IdUtakmica,IdIgrac,Vrsta,Kos)
+VALUES
+(21,2,31,1,TRUE),
+(22,2,31,1,TRUE),
+(23,2,31,2,FALSE),
+(24,2,31,3,TRUE),
+(25,2,31,3,FALSE),
+(26,2,33,1,FALSE),
+(27,2,33,1,TRUE),
+(28,2,33,2,TRUE),
+(29,2,33,2,TRUE),
+(30,2,33,3,TRUE),
+(31,2,46,1,TRUE),
+(32,2,46,1,TRUE),
+(33,2,46,2,FALSE),
+(34,2,46,2,FALSE),
+(35,2,46,3,TRUE),
+(36,2,47,2,TRUE),
+(37,2,47,2,TRUE),
+(38,2,47,3,TRUE),
+(39,2,47,3,FALSE),
+(40,2,47,3,TRUE);
+INSERT INTO Sut(Id,IdUtakmica,IdIgrac,Vrsta,Kos)
+VALUES
+(41,31,1,1,FALSE),
+(42,31,1,1,TRUE),
+(43,31,1,2,TRUE),
+(44,31,1,3,TRUE),
+(45,31,1,3,FALSE),
+(46,31,3,1,FALSE),
+(47,31,3,1,TRUE),
+(48,31,3,2,FALSE),
+(49,31,3,2,TRUE),
+(50,31,3,3,TRUE),
+(51,31,91,1,TRUE),
+(52,31,91,1,TRUE),
+(53,31,91,2,TRUE),
+(54,31,91,2,TRUE),
+(55,31,91,3,FALSE),
+(56,31,92,1,TRUE),
+(57,31,92,2,TRUE),
+(58,31,92,3,TRUE),
+(59,31,92,3,FALSE),
+(60,31,92,3,TRUE);
+
+INSERT INTO Prekrsaj(Id,IdUtakmica,IdIgracRadi,IdIgracTrpi,Iskljucenje)
+VALUES
+(1,1,1,16,FALSE),
+(2,1,2,17,FALSE),
+(3,1,3,18,FALSE),
+(4,1,3,16,FALSE),
+(5,1,3,17,FALSE),
+(6,1,16,1,FALSE),
+(7,1,17,1,FALSE),
+(8,1,19,5,TRUE),
+(9,1,19,4,FALSE),
+(10,1,20,3,FALSE);
+INSERT INTO Prekrsaj(Id,IdUtakmica,IdIgracRadi,IdIgracTrpi,Iskljucenje)
+VALUES
+(11,2,31,46,FALSE),
+(12,2,32,47,FALSE),
+(13,2,33,48,FALSE),
+(14,2,33,46,TRUE),
+(15,2,33,47,FALSE),
+(16,2,46,31,FALSE),
+(17,2,47,31,FALSE),
+(18,2,49,35,TRUE),
+(19,2,49,34,FALSE),
+(20,2,50,33,FALSE);
+INSERT INTO Prekrsaj(Id,IdUtakmica,IdIgracRadi,IdIgracTrpi,Iskljucenje)
+VALUES
+(21,31,1,91,FALSE),
+(22,31,2,92,FALSE),
+(23,31,3,93,FALSE),
+(24,31,3,94,FALSE),
+(25,31,3,95,TRUE),
+(26,31,91,1,FALSE),
+(27,31,92,1,FALSE),
+(28,31,93,5,FALSE),
+(29,31,94,4,FALSE),
+(30,31,95,3,FALSE);
+
+INSERT INTO Skok(Id,IdUtakmica,IdIgrac,Defenzivni)
+VALUES
+(1,1,1,FALSE),
+(2,1,1,FALSE),
+(3,1,2,TRUE),
+(4,1,2,FALSE),
+(5,1,3,TRUE),
+(6,1,3,FALSE),
+(7,1,4,FALSE),
+(8,1,4,TRUE),
+(9,1,4,FALSE),
+(10,1,5,FALSE),
+(11,1,16,TRUE),
+(12,1,16,TRUE),
+(13,1,17,TRUE),
+(14,1,17,FALSE),
+(15,1,18,TRUE),
+(16,1,18,FALSE),
+(17,1,18,TRUE),
+(18,1,19,FALSE),
+(19,1,20,TRUE),
+(20,1,20,FALSE);
+INSERT INTO Skok(Id,IdUtakmica,IdIgrac,Defenzivni)
+VALUES
+(21,2,31,FALSE),
+(22,2,31,FALSE),
+(23,2,32,TRUE),
+(24,2,32,FALSE),
+(25,2,33,TRUE),
+(26,2,33,FALSE),
+(27,2,34,FALSE),
+(28,2,34,TRUE),
+(29,2,34,FALSE),
+(30,2,35,FALSE),
+(31,2,46,TRUE),
+(32,2,46,TRUE),
+(33,2,47,TRUE),
+(34,2,47,FALSE),
+(35,2,48,TRUE),
+(36,2,48,FALSE),
+(37,2,48,TRUE),
+(38,2,49,FALSE),
+(39,2,50,TRUE),
+(40,2,50,FALSE);
+INSERT INTO Skok(Id,IdUtakmica,IdIgrac,Defenzivni)
+VALUES
+(41,31,1,FALSE),
+(42,31,1,FALSE),
+(43,31,2,TRUE),
+(44,31,2,FALSE),
+(45,31,3,TRUE),
+(46,31,3,FALSE),
+(47,31,4,FALSE),
+(48,31,4,TRUE),
+(49,31,4,FALSE),
+(50,31,5,FALSE),
+(51,31,91,TRUE),
+(52,31,91,TRUE),
+(53,31,92,TRUE),
+(54,31,92,FALSE),
+(55,31,92,TRUE),
+(56,31,93,FALSE),
+(57,31,93,TRUE),
+(58,31,94,FALSE),
+(59,31,94,TRUE),
+(60,31,95,FALSE);
+
+INSERT INTO Ukradena(Id,IdUtakmica,IdIgracKrade,IdIgracGubi)
+VALUES
+(1,1,1,16),
+(2,1,1,20),
+(3,1,2,18),
+(4,1,3,17),
+(5,1,4,16),
+(6,1,16,1),
+(7,1,17,2),
+(8,1,18,1),
+(9,1,18,3),
+(10,1,19,5);
+INSERT INTO Ukradena(Id,IdUtakmica,IdIgracKrade,IdIgracGubi)
+VALUES
+(11,2,31,46),
+(12,2,31,50),
+(13,2,32,48),
+(14,2,33,47),
+(15,2,34,46),
+(16,2,46,31),
+(17,2,47,32),
+(18,2,48,31),
+(19,2,48,33),
+(20,2,49,35);
+INSERT INTO Ukradena(Id,IdUtakmica,IdIgracKrade,IdIgracGubi)
+VALUES
+(21,31,1,91),
+(22,31,1,92),
+(23,31,2,92),
+(24,31,3,93),
+(25,31,4,94),
+(26,31,91,1),
+(27,31,91,2),
+(28,31,92,3),
+(29,31,93,4),
+(30,31,95,5);
+
+--kronološki pregled odigranih utakmica
+SELECT Domacin.Ime AS Domacin, Gost.Ime AS Gost, Sudac.Ime AS Sudac, Datum, PoeniDomacina,PoeniGosta,FazaNatjecanja
+FROM Utakmica INNER JOIN Tim Domacin 
+ON Utakmica.IdDomacin=Domacin.Id INNER JOIN Tim Gost
+ON Utakmica.IdGost=Gost.Id INNER JOIN Sudac
+ON Utakmica.IdSudac=Sudac.Id
+ORDER BY Datum
+
+--popis svih igrača koji igrali za određenu momčad tijekom sezone
+SELECT DISTINCT Ime FROM Igrac INNER JOIN Zamjena 
+ON Igrac.Id=Zamjena.IdIgracUlazi
+WHERE Igrac.IdTim=(SELECT Id FROM Tim WHERE Ime ='Boston Celtics')
+
+--popis igrača koji su igrali na pojedinoj utakmici.
+SELECT DISTINCT ime AS igrac FROM zamjena INNER JOIN igrac ON zamjena.idigraculazi = igrac.id
+WHERE idigraculazi IS NOT NULL AND idutakmica = 1
+
+--izvještaj o rezultatima svih utakmica koje je određena momčad odigrala u pojedinoj fazi natjecanja
+SELECT Domacin.Ime, Gost.Ime, PoeniDomacina, PoeniGosta, FazaNatjecanja 
+FROM Utakmica INNER JOIN Tim Domacin 
+ON Utakmica.IdDomacin=Domacin.Id INNER JOIN Tim Gost
+ON Utakmica.IdGost=Gost.Id
+WHERE (IdDomacin=(SELECT Id FROM Tim WHERE Ime='Boston Celtics')
+OR IdGost=(SELECT Id FROM Tim WHERE Ime='Boston Celtics'))
+AND FazaNatjecanja='Regularna sezona'
+
+--izvještaj o ulascima i izlascima igrača na pojedinoj utakmici
+SELECT Ulazi.Ime as Ulazi, Izlazi.Ime as Izlazi, Zamjena.Minuta 
+FROM Igrac Ulazi INNER JOIN Zamjena 
+ON Zamjena.IdIgracUlazi=Ulazi.Id INNER JOIN Igrac Izlazi 
+ON Zamjena.IdIgracIzlazi=Izlazi.Id 
+WHERE Zamjena.IdIgracUlazi IS NOT NULL AND Zamjena.IdIgracIzlazi IS NOT NULL AND Zamjena.IdUtakmica=2
+
+--izvještaj o prekršajima suđenim na pojedinoj utakmici
+SELECT Radi.Ime as RadiPrekrsaj, Trpi.Ime as TrpiPrekrsaj 
+FROM Igrac Radi INNER JOIN Prekrsaj 
+ON Prekrsaj.IdIgracRadi=Radi.Id INNER JOIN Igrac Trpi 
+ON Prekrsaj.IdIgracTrpi=Trpi.Id 
+WHERE Prekrsaj.IdUtakmica=31
+
+--statistiku učinka određenog igrača na pojedinoj utakmici, gdje učinak uključuje 
+--podatke o ukupnom broju postignutih koševa, broju koševa za 1 bod, broju koševa za 
+--2 boda, broju „trica“, postotnoj uspješnosti za svaku navedenu stavku, broju osvojenih 
+--lopti, broju izgubljenih lopti, broju skokova te broju prekršaja.
+WITH statistikaSut AS(
+	SELECT ime, idutakmica, vrsta, kos, COUNT(*) AS broj_suteva
+	FROM igrac INNER JOIN sut ON igrac.id = sut.idigrac
+	WHERE idigrac = 3 AND idutakmica = 1
+	GROUP BY 1, 2, 3, 4),
 	
-SELECT COUNT(*) FROM Igrac
-			WHERe IdTim = 1
+	statistikaPrekrsaj AS(
+	SELECT ime, idutakmica
+	FROM igrac INNER JOIN prekrsaj ON igrac.id=prekrsaj.idigracradi
+	WHERE idigracradi = 3 AND idutakmica = 1
+	),
+	
+	statistikaUkradena AS(
+	SELECT ime, idutakmica
+	FROM igrac INNER JOIN ukradena ON igrac.id=ukradena.idigrackrade
+	WHERE idigrackrade = 3 AND idutakmica = 1
+	),
+	
+	statistikaIzgubljena AS(
+	SELECT ime, idutakmica
+	FROM igrac INNER JOIN ukradena ON igrac.id=ukradena.idigracgubi
+	WHERE idigracgubi = 3 AND idutakmica = 1
+	),
+	
+	statistikaSkok AS(
+	SELECT ime, idutakmica
+	FROM igrac INNER JOIN skok ON igrac.id=skok.idigrac
+	WHERE idigrac = 3 AND idutakmica = 1
+	)
+
+SELECT ime, idutakmica,
+	SUM(CASE WHEN kos = true THEN broj_suteva ELSE 0 END) AS broj_koseva,
+	ROUND(SUM(CASE WHEN kos = true THEN broj_suteva ELSE 0 END) / SUM(broj_suteva), 2) * 100 AS postotak_kos,
+	SUM(CASE WHEN vrsta = 1 AND kos = true THEN broj_suteva ELSE 0 END) AS br_kos_za_1,
+	ROUND(SUM(CASE WHEN vrsta = 1 AND kos = true THEN broj_suteva ELSE 0 END) / SUM(CASE WHEN vrsta = 1 THEN broj_suteva ELSE 0 END), 2) * 100 AS postotak_za_1,
+	SUM(CASE WHEN vrsta = 2 AND kos = true THEN broj_suteva ELSE 0 END) AS br_kos_za_2,
+	ROUND(SUM(CASE WHEN vrsta = 2 AND kos = true THEN broj_suteva ELSE 0 END) / SUM(CASE WHEN vrsta = 2 THEN broj_suteva ELSE 0 END), 2) * 100 AS postotak_za_2,
+	SUM(CASE WHEN vrsta = 3 AND kos = true THEN broj_suteva ELSE 0 END) AS br_kos_za_3,
+	ROUND(SUM(CASE WHEN vrsta = 3 AND kos = true THEN broj_suteva ELSE 0 END) / SUM(CASE WHEN vrsta = 3 THEN broj_suteva ELSE 0 END), 2) * 100 AS postotak_za_3,
+	(SELECT COUNT(*) FROM statistikaPrekrsaj) AS broj_prekrsaja,
+	(SELECT COUNT(*) FROM statistikaUkradena) AS broj_ukradenih_lopti,
+	(SELECT COUNT(*) FROM statistikaIzgubljena) AS broj_izgubljenih_lopti,
+	(SELECT COUNT(*) FROM statistikaSkok) AS broj_skokova
+FROM statistikaSut
+GROUP BY 1,2
+
+--prosječnu statistiku učinka određenog igrača u cijeloj sezoni te ukupno vrijeme koje 
+--proveo u igri.
+WITH statistikaSut AS(
+	SELECT ime, vrsta, kos
+	FROM igrac INNER JOIN sut ON sut.idigrac=igrac.id
+	WHERE igrac.id=3
+),
+	statistikaMinute AS(
+	SELECT ime, igrac.id,idigraculazi, idigracizlazi, minuta 
+	FROM igrac INNER JOIN zamjena ON igrac.id=zamjena.idigraculazi OR igrac.id=zamjena.idigracizlazi
+	WHERE igrac.id=3
+)
+
+SELECT
+	ime,
+	COUNT(*) AS broj_suteva,
+	COUNT(CASE WHEN kos=true THEN kos END) AS broj_koseva,
+	COUNT(CASE WHEN kos=true THEN kos END)::FLOAT/COUNT(*)*100 || '%' AS postotak_ucinka,
+	(SELECT SUM(CASE WHEN idigracizlazi=id THEN minuta END) - SUM(CASE WHEN idigraculazi=id THEN minuta END) AS ukupne_minute_igranja FROM statistikaMinute)
+	FROM statistikaSut
+	GROUP BY 1
+
+-- pregled rezultata svih odigranih utakmica.
+SELECT Domacin.Ime AS Domacin, Gost.Ime AS Gost, Sudac.Ime AS Sudac, Datum, PoeniDomacina,PoeniGosta,FazaNatjecanja
+FROM Utakmica INNER JOIN Tim Domacin 
+ON Utakmica.IdDomacin=Domacin.Id INNER JOIN Tim Gost
+ON Utakmica.IdGost=Gost.Id INNER JOIN Sudac
+ON Utakmica.IdSudac=Sudac.Id
+ORDER BY Datum
+
+CREATE VIEW BrojIgracaPoTimu AS
+SELECT
+    Tim.id AS TimId,
+    Tim.ime AS TimIme,
+    COUNT(Igrac.id) AS BrojIgraca
+FROM
+    Tim 
+    LEFT JOIN Igrac  ON Tim.id = Igrac.IdTim
+GROUP BY
+    Tim.id, Tim.ime;
+
+CREATE VIEW ProsjecniPoeniPoTimu AS
+SELECT
+    Tim.id AS TimId,
+    Tim.ime AS TimIme,
+    AVG(CASE WHEN Tim.id = Utakmica.IdDomacin THEN Utakmica.PoeniDomacina END) AS ProsjecniPoeniKaoDomacini,
+    AVG(CASE WHEN Tim.id = Utakmica.IdGost THEN Utakmica.PoeniGosta END) AS ProsjecniPoeniKaoGosti
+FROM
+    Tim 
+    LEFT JOIN Utakmica ON Tim.id = Utakmica.IdDomacin OR Tim.id = Utakmica.IdGost
+GROUP BY
+    Tim.id, Tim.ime;
+
